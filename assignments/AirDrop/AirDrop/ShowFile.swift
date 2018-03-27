e5dc9eafdcb9d59fa1868a2b4a8dff56b7574364
Mijeong Ban <mban1@stevens.edu>
2018-03-27 18:05:43 -0400

    update

--- assignments/AirDrop/AirDrop/ShowFile.swift
+++ assignments/AirDrop/AirDrop/ShowFile.swift
@@ -5,5 +5,43 @@
 //  Created by Mijeong Ban on 3/21/18.
 //  Copyright © 2018 Mijeong Ban. All rights reserved.
 //
+//  I pledge my honor that I have abided by the Stevens Honor System
 
-import Foundation
+
+import UIKit
+
+class ShowFile: UIViewController {
+    
+    @IBOutlet weak var share: UIButton!
+    var fileName : String = ""
+    var localFilePath : URL! = nil
+    @IBOutlet weak var wev: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        share.layer.cornerRadius = 5.0
+        share.layer.masksToBounds = true
+        
+        let arrComponent = fileName.components(separatedBy: ".")
+        let filePath = Bundle.main.path(forResource: arrComponent[0], ofType:arrComponent[1])
+        
+        localFilePath = URL(fileURLWithPath: filePath!)
+        let request = URLRequest(url: localFilePath)
+        wev.loadRequest(request)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func shareAction(_ sender: Any) {
+        let objShare = [localFilePath]
+        let objActivityController = UIActivityViewController(activityItems: objShare, applicationActivities: nil)
+        let excludedActivities = [UIActivityType.postToTwitter, UIActivityType.postToFacebook, UIActivityType.postToWeibo, UIActivityType.message, UIActivityType.mail, UIActivityType.print, UIActivityType.copyToPasteboard, UIActivityType.assignToContact, UIActivityType.saveToCameraRoll, UIActivityType.addToReadingList, UIActivityType.postToFlickr, UIActivityType.postToVimeo, UIActivityType.postToTencentWeibo]
+        objActivityController.excludedActivityTypes = excludedActivities
+        self.present(objActivityController, animated: true)
+    }
+    
+}
+
--- assignments/AirDrop/AirDrop/TableViewController.swift
+++ assignments/AirDrop/AirDrop/TableViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Mijeong Ban on 3/21/18.
 //  Copyright © 2018 Mijeong Ban. All rights reserved.
 //
+//  I pledge my honor that I have abided by the Stevens Honor System
 
 import Foundation
 import UIKit
@@ -12,10 +13,11 @@ import UIKit
 class TableViewController : UITableViewController {
     
     var names = [String]()
+    var identities = [String]()
     
     override func viewDidLoad() {
-        
         names = ["IMAGE", "PDF", "TEXT"]
+        identities = ["F", "F", "F"]
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -31,6 +33,15 @@ class TableViewController : UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        <#code#>
+        let identity = identities[indexPath.row]
+        let viewController = storyboard?.instantiateViewController(withIdentifier: identity) as! ShowFile
+        if (indexPath.row == 0) {
+            viewController.fileName = "image.png"
+        } else if (indexPath.row == 1) {
+            viewController.fileName = "pdf.pdf"
+        } else {
+            viewController.fileName = "text.txt"
+        }
+        self.navigationController?.pushViewController(viewController, animated: true)
     }
 }
